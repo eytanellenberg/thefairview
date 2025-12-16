05c8b6cffbe18c75cdc91bb512a4e8f1d40250c6
eytanellenberg <eytan_ellenberg@yahoo.fr>
2025-12-16 10:16:22 +0200

    Create utils.ts

---
+++ lib/rai/utils.ts
@@ -0,0 +1,5 @@
+export function daysBetween(a: string, b: string) {
+  const d1 = new Date(a).getTime();
+  const d2 = new Date(b).getTime();
+  return Math.round((d2 - d1) / (1000 * 60 * 60 * 24));
+}
