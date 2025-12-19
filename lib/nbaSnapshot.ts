e76ca78c5864a3d2f3656db101d29d3e383b3097
eytanellenberg <eytan_ellenberg@yahoo.fr>
2025-12-19 11:06:45 +0200

    Update nbaSnapshot.ts

--- lib/nbaSnapshot.ts
+++ lib/nbaSnapshot.ts
@@ -1,3 +1,6 @@
+export async function buildNBASnapshot() {
+  throw new Error("SNAPSHOT EXECUTED");
+}
 import { NBA_TEAMS } from "@/lib/data/nbaTeams";
 import { getLastAndNextGame } from "@/lib/providers/espn";
 
